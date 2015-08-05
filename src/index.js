443cfe8372ca1601e9a94a32eb7905d3e5092b01
Naman Goel <naman34@gmail.com>
2015-08-05 18:44:35 +0200

    Added rule: modal-no-string-literals, Specific to Upclose.me

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@
 
 import jsxNoStringLiteral from './rules/jsx-no-string-literals'
 import reactExports from './rules/no-untyped-react-exports'
+import modalNoStringLiterals from './rules/modal-no-string-literals'
 
 export default
   { rules:
       { 'jsx-no-string-literals': jsxNoStringLiteral
       , 'no-untyped-react-exports': reactExports
+      , 'modal-no-string-literals': modalNoStringLiterals
       }
   , rulesConfig:
       { 'jsx-no-string-literals': 1
       , 'no-untyped-react-exports': 1
+      , 'modal-no-string-literals': 1
       }
   }
---
+++ src/rules/modal-no-string-literals.js
@@ -0,0 +1,49 @@
+/**
+ * @fileoverview Disallows using string literals in JSX to enforce localization.
+ * @author Naman Goel
+ */
+/* @flow */
+import type {Context, Rules, EslintNode} from './types'
+
+const numberSpace = /^[\s0-9]*$/
+const isLiteral = child => child.type === 'Literal'
+const isNotEmpty = literalNode => !numberSpace.test(literalNode.value)
+// ------------------------------------------------------------------------------
+// Rule Definition
+// ------------------------------------------------------------------------------
+
+export default function(context: Context): Rules {
+  return {
+    CallExpression: function(node: EslintNode): void {
+      if(!(node.callee.type === 'MemberExpression'
+        && node.callee.object.type === 'Identifier'
+        && node.callee.object.name === 'ee'
+        && node.callee.property.type === 'Identifier'
+        && node.callee.proper.name == 'emit')){
+        return
+      }
+      if(node.arguments.length !== 2){
+        return
+      }
+      if(node.arguments[0].type !== 'Literal'){
+        return
+      }
+      const eventName = node.arguments[0].value
+      if(eventName !== 'showModal' && eventName !== 'showBanner'){
+        return
+      }
+      if(node.arguments[1].type !== 'ObjectExpression'){
+        return
+      }
+
+      node.arguments[1].properties
+        .filter(prop => prop.key.type === 'Identifier')
+        .filter(prop => prop.key.name === 'title' || prop.key.name === 'body' || prop.key.name === 'message')
+        .filter(prop => prop.value.type === 'Literal')
+        .forEach(prop => context.report(prop, 'Use L10ns not a string literal'))
+    }
+  }
+
+}
+
+export const schema: Array<any> = []
